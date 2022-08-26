ad7cfbbfabec48fc327b76d3fdea66071f5cd284
7dog123 <futuretrunks745@gmail.com>
2022-08-26 09:49:38 +0000

    updated everything

--- src/main/arch.c
+++ src/main/arch.c
@@ -1,4 +1,8 @@
 
 /* This is needed for automake, so that it can build an empty archive */
 
-int __round42;
+#ifdef _EE
+  int __round42;
+#else
+  int pointless_symbol_so_the_archive_doesnt_look_empty;
+#endif
